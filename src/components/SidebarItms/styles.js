f1cb393312cb5336c262f7a36874892f333c218d
Arlisson N. Costa <arlisson314@gmail.com>
2023-04-23 15:19:14 -0300

    style: SidebarItems

--- src/components/SidebarItms/styles.js
+++ src/components/SidebarItms/styles.js
@@ -1,22 +1,15 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  display: flex;
   align-items: center;
-  background-color: var(--terciary); 
-  font-size: 20px;
-  color: var(--secundary);
-  padding: 10px;
-  cursor: pointer;
-  border-radius: 10px;
   margin: 0 15px 20px;
-
+  text-align: center;
+  font-size: 20px;
+  cursor: pointer;
+  display: flex;
+  padding: 10px;
   > svg {
     margin: 0 20px;
   }
-
-  /* &:hover {
-    background-color: black;
-  } */
 `;
 export default Container;
